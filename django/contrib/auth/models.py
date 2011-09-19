b280d1802c626706328752e5d32aa4a8269f4141
Yarko Tymciurak <yarkot@uchicago.edu>
2011-09-19 11:23:35 -0500

    update contrib auth to use django.utils.tokens
    Conflicts:
    
    	django/contrib/auth/models.py
    	django/contrib/auth/tests/basic.py
    	django/contrib/auth/utils.py

--- django/contrib/auth/models.py
+++ django/contrib/auth/models.py
@@ -6,16 +6,17 @@ from django.core.exceptions import ImproperlyConfigured
 from django.core.mail import send_mail
 from django.db import models
 from django.db.models.manager import EmptyManager
-from django.utils.crypto import get_random_string
 from django.utils.encoding import smart_str
+from django.utils.tokens import RandomToken
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 
 from django.contrib import auth
 # UNUSABLE_PASSWORD is still imported here for backwards compatibility
-from django.contrib.auth.hashers import (
-    check_password, make_password, is_password_usable, UNUSABLE_PASSWORD)
 from django.contrib.auth.signals import user_logged_in
+from django.contrib.auth.utils import (get_hexdigest, make_password,
+        check_password, is_password_usable)
+
 from django.contrib.contenttypes.models import ContentType
 
 
@@ -180,7 +181,9 @@ class UserManager(models.Manager):
         have "I" or "O" or letters and digits that look similar -- just to
         avoid confusion.
         """
-        return get_random_string(length, allowed_chars)
+        # Note that default value of allowed_chars does not have "I", "0", or letters
+        # that look like them -- just to avoid confusion.
+        return RandomToken().readable_alphabet(length=length)
 
     def get_by_natural_key(self, username):
         return self.get(username=username)
@@ -291,21 +294,28 @@ class User(models.Model):
         return full_name.strip()
 
     def set_password(self, raw_password):
-        self.password = make_password(raw_password)
+        self.password = make_password('sha256', raw_password)
 
     def check_password(self, raw_password):
         """
         Returns a boolean of whether the raw_password was correct. Handles
         hashing formats behind the scenes.
         """
-        def setter(raw_password):
-            self.set_password(raw_password)
-            self.save()
-        return check_password(raw_password, self.password, setter)
+        # Backwards-compatibility check. Older passwords won't include the
+        # algorithm or salt.
+        if '$' not in self.password:
+            # for tests also - this is the default algo
+            is_correct = (self.password == get_hexdigest('md5', '', raw_password))
+            if is_correct:
+                # Convert the password to the new, more secure format.
+                self.set_password(raw_password)
+                self.save()
+            return is_correct
+        return check_password(raw_password, self.password)
 
     def set_unusable_password(self):
         # Sets a value that will never be a valid hash
-        self.password = make_password(None)
+        self.password = make_password('sha256', None)
 
     def has_usable_password(self):
         return is_password_usable(self.password)
